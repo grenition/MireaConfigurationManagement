f4bf9f4974bec7dc09faae1bc1154d42c734df71
Arssseniy <88106737+grenition@users.noreply.github.com>
2024-11-21 20:27:58 +0300

    refactor: Reorganize folders

---
+++ src/ConfLanguage/ConfLanguageScenario.cs
@@ -0,0 +1,13 @@
+using MireaConfigurationManagement.Core.Scenarios;
+
+namespace MireaConfigurationManagement.ConfLanguage;
+
+public class ConfLanguageScenario : IScenario
+{
+    public string Key => "conf_language";
+    
+    public async Task Execute(CancellationToken token)
+    {
+        
+    }
+}
